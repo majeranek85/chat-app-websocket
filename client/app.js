9fb5bca59c8e43b4e5f893387ad8f576428fbfbc
Marta Majerowska <majeranek85@gmail.com>
2020-07-19 18:04:52 +0200

    Add login validation & messages section display

--- client/app.js
+++ client/app.js
@@ -1,8 +1,23 @@
-const loginForm = document.getElementById('#welcome-form');
-const messagesSection = document.getElementById('#message-section');
-const messagesList = document.getElementById('#message-list');
-const addMessageForm = document.getElementById('#add-messages-form');
-const userNameInput = document.getElementById('#username');
-const messageContentInput = document.getElementById('#message-content');
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('message-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
 
 let userName;
+
+
+const login = (e) => {
+  e.preventDefault();
+
+  if (userNameInput.value == ''){
+      window.alert('Please write your login')
+  } else {
+      userName = userNameInput.value;
+      loginForm.classList.remove('show');
+      messagesSection.classList.add('show');
+  }
+};
+
+loginForm.addEventListener('submit', login);
