58e91c98bd8b926662790f3749ae5c23fcbc2e8e
Marta Majerowska <majeranek85@gmail.com>
2020-07-19 19:26:26 +0200

    Add addMessage function

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
-const messagesList = document.getElementById('message-list');
+const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
-const messageContentInput = document.getElementById('message-content');
+let messageContentInput = document.getElementById('message-content');
 
 let userName;
 
@@ -12,7 +12,7 @@ const login = (e) => {
   e.preventDefault();
 
   if (userNameInput.value == ''){
-      window.alert('Please write your login')
+      window.alert('Please write your login');
   } else {
       userName = userNameInput.value;
       loginForm.classList.remove('show');
@@ -20,4 +20,28 @@ const login = (e) => {
   }
 };
 
+const addMessage = (name, message) => {
+  const listElem = document.createElement('li')//
+  listElem.classList.add('message', 'message--received', `${name === userName && `message--self`}`);
+  console.log(listElem);
+  listElem.innerHTML = `
+    <h3 class='message__author'>${name === userName ? 'You' : name}</h3>
+    <div class='message__content'>${message}</div>
+  `;
+
+  messagesList.appendChild(listElem);
+};
+
+const sendMessage = (e) => {
+  e.preventDefault();
+
+  if (messageContentInput.value == ''){
+    window.alert('Add your message')
+  } else {
+    addMessage(userName, messageContentInput.value);
+    messageContentInput.value = '';
+  }
+};
+
 loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
