69cfad6304258585d7ca5a468c83420f7f18d641
Marta Majerowska <majeranek85@gmail.com>
2020-07-19 14:22:09 +0200

    Add ref to html file

---
+++ client/app.js
@@ -0,0 +1,8 @@
+const loginForm = document.getElementById('#welcome-form');
+const messagesSection = document.getElementById('#message-section');
+const messagesList = document.getElementById('#message-list');
+const addMessageForm = document.getElementById('#add-messages-form');
+const userNameInput = document.getElementById('#username');
+const messageContentInput = document.getElementById('#message-content');
+
+let userName;
